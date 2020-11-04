f10b06cdc5d19d034a5e46aa53174cd84b607231
FORodrigo <vc.rodrigofeito@gmail.com>
2020-11-04 20:39:45 -0300

    Agregando archivos

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+
+const app ) express ()
+
+
+app.get ('./', function (req, res){
+
+    res.sendFile(__dirname + 'views/index.html')
+})
+
+app.listen(3000, function(){
+
+    console.log('para ver el sitio ingresa a http://localhosto:3000')
+})
